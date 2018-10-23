7d7334729088f557560a49ec7edf3b8f7d911876
Nicanor008 <nicanorkorir008@gmail.com>
2018-10-23 17:56:29 +0300

    [ch-#161414646]database setup file

---
+++ app/db.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+url = "dbname='storemanager' host='localhost' port='5432' user='postgres' password=''"
+
+def connection(url):
+    con = psycopg2.connect(url)
+    return con
+def init_db():
+    con = connection(url)
+    return con
+
+def create_tables():
+    conn = connection(url)
+    curr = conn.cursor()
+    queries = tables()
+
+    for query in queries:
+        curr.execute(query)
+    conn.commit()
+
+def tables():
+    users = """CREATE TABLE users(
+        employeeno INT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        email CHAR(64) NOT NULL,
+        password CHAR(64) NOT NULL
+    )"""
+
+    products = """CREATE TABLE products(
+        productid INT PRIMARY KEY NOT NULL,
+        product_category TEXT NOT NULL,
+        product_name TEXT NOT NULL,
+        product_quantity TEXT NOT NULL,
+        price INT NOT NULL
+    )"""
+    
+    sales = """CREATE TABLE sales(
+        salesid INT PRIMARY KEY NOT NULL,
+        product_category TEXT NOT NULL,
+        product_name TEXT NOT NULL,
+        product_quantity TEXT NOT NULL,
+        price INT NOT NULL
+        attended_by INT NOT NULL
+    )"""
+
+    queries = [users, products, sales]
+    return queries
