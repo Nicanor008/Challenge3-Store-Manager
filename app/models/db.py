6bc4372c19200ea0d214413cf6838e4a306d5c6b
Nicanor008 <nicanorkorir008@gmail.com>
2018-11-11 11:13:54 +0300

    fixing heroku

--- app/models/db.py
+++ app/models/db.py
@@ -6,7 +6,8 @@ enviroment = os.environ['ENV']
 
 url = os.getenv('DATABASE_URL')
 def connection(url):
-    con = psycopg2.connect(app_config[enviroment].DATABASE_URL)
+    DATABASE_URL = os.environ['DATABASE_URL']
+    con = psycopg2.connect(DATABASE_URL, sslmode='require')
     return con
 def init_db():
     con = connection(url)
